a7382990c248fb145e740a8eca5d63c9d39d714d
JacquelineLK119 <jflores43@ucol.mx>
2019-11-07 13:10:43 -0600

    Correcciones, faltan mejoras.

--- ColasFIFOAC/js/main.js
+++ ColasFIFOAC/js/main.js
@@ -1,6 +1,12 @@
+import Procesador from './Procesador.js';
+
 class Main {
     constructor() {
+        let procesador = new Procesador();
 
+        document.querySelector("#btnComenzar").addEventListener("click", () => {
+            procesador.comenzar();
+        });
 
     }
 }
